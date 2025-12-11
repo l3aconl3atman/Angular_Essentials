080b8088aa7d1db40dba53a44e134e7ceb5efea2
Jethro Moey <jmse97@gmail.com>
2025-12-11 21:15:53 +0800

    Angular 02 Essentials: 45 Outputting User-specific Tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -9,6 +9,36 @@ import { TaskComponent } from './task/task.component';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  name = input<string | undefined>();
-  avatar = input<string>();
+  userId = input.required<string>();
+  name = input.required<string | undefined>();
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId == this.userId());
+  }
 }
