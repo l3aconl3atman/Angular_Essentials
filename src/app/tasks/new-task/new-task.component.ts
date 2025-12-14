47b9129943beb90153505755acb1f68d25c35145
Jethro Moey <jmse97@gmail.com>
2025-12-14 15:14:23 +0800

    Angular 02 Essentials: 52 Using Directives & Two-Way-Binding

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,18 @@
 import { Component, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   exitAddTask = output();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   onExitAddTask() {
     this.exitAddTask.emit();
